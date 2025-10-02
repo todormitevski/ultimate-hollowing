90bf4e183f3fbd12d1a916f0f66d962bacb4de89
todormitevski <todormitevski5@gmail.com>
2025-10-02 03:32:52 +0200

    refactor: refactor suspense and styles
    - Fix "flickering" UI issue
    - Improve responsivity by adding more media queries

--- src/components/CharacterClass.tsx
+++ src/components/CharacterClass.tsx
@@ -1,8 +1,6 @@
 import { ReactNode } from "react";
 import classes from "./CharacterClass.module.css";
 
-import placeholder from "../assets/images/character-classes/placeholder.png";
-
 type Props = {
   name: string;
   imgUrl: string;
@@ -14,11 +12,13 @@ export default function CharacterClass({ name, imgUrl, imgAlt }: Props) {
   return (
     <div className={classes.characterContainer}>
       <h4 className={classes.characterName}>{name}</h4>
-      <img
-        src={imgUrl || placeholder}
-        alt={imgAlt}
-        className={classes.characterImage}
-      />
+      <div className={classes.imageContainer}>
+        <img
+          src={imgUrl}
+          alt={imgAlt}
+          className={classes.characterImage}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/LoadingIndicator.tsx
@@ -0,0 +1,5 @@
+import classes from "./LoadingIndicator.module.css";
+
+export const LoadingIndicator = () => {
+  return <span className={classes.loader}></span>;
+};
